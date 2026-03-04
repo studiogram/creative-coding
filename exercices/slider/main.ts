e72589f842c3f9b1172fb691bf70b85fee24d6fd
studiogram <contact@studio-gram.com>
2026-03-04 15:22:36 +0100

    Add slider exercice

---
+++ exercices/slider/main.ts
@@ -0,0 +1,51 @@
+import "@/style.css";
+import "./style.css";
+import Swiper from "swiper";
+import { Navigation, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+
+const slides = document.querySelectorAll(".macarons .swiper-slide");
+
+const slider = new Swiper(".macarons", {
+  slidesPerView: 3,
+  loop: true,
+  modules: [Navigation],
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+slider.on("slideChange", function () {
+  const currentIndex =
+    slider.realIndex + 1 < slider.slides.length ? slider.realIndex + 1 : 0;
+
+  console.log(currentIndex);
+
+  const sliderEl = document.getElementById("slider");
+  const sliderCircle = document.getElementById("circle");
+  const backgroundColor =
+    slider.slides[currentIndex].getAttribute("data-background");
+  const circleColor = slider.slides[currentIndex].getAttribute("data-circle");
+  if (sliderEl && backgroundColor) {
+    sliderEl.style.background = backgroundColor;
+  }
+  if (sliderCircle && circleColor) {
+    sliderCircle.style.background = circleColor;
+  }
+});
+
+const loopSlider = new Swiper(".slider", {
+  slidesPerView: 3,
+  spaceBetween: 120,
+  loop: true,
+  speed: 4000,
+  modules: [Autoplay],
+  allowTouchMove: false,
+  autoplay: {
+    delay: 1,
+    disableOnInteraction: false,
+  },
+});
