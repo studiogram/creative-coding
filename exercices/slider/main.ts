e235cca43b704aa6c7f36084bfc78d5276171b09
studiogram <contact@studio-gram.com>
2026-03-04 16:06:57 +0100

    Fix slider exercice colors issue

--- exercices/slider/main.ts
+++ exercices/slider/main.ts
@@ -6,8 +6,8 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/autoplay";
 
+/* Slider des macarons */
 const slides = document.querySelectorAll(".macarons .swiper-slide");
-
 const slider = new Swiper(".macarons", {
   slidesPerView: 3,
   loop: true,
@@ -21,14 +21,11 @@ const slider = new Swiper(".macarons", {
 slider.on("slideChange", function () {
   const currentIndex =
     slider.realIndex + 1 < slider.slides.length ? slider.realIndex + 1 : 0;
-
-  console.log(currentIndex);
-
   const sliderEl = document.getElementById("slider");
   const sliderCircle = document.getElementById("circle");
-  const backgroundColor =
-    slider.slides[currentIndex].getAttribute("data-background");
-  const circleColor = slider.slides[currentIndex].getAttribute("data-circle");
+  const backgroundColor = slides[currentIndex].getAttribute("data-background");
+  const circleColor = slides[currentIndex].getAttribute("data-circle");
+  /* Modifications des couleurs */
   if (sliderEl && backgroundColor) {
     sliderEl.style.background = backgroundColor;
   }
@@ -37,7 +34,8 @@ slider.on("slideChange", function () {
   }
 });
 
-const loopSlider = new Swiper(".slider", {
+/* Slider en boucle */
+new Swiper(".slider", {
   slidesPerView: 3,
   spaceBetween: 120,
   loop: true,
